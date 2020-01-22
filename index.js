31c276ddcbe8e3270be4dcb229e4d2799e5a336f
ohn582 <49073115+ohn582@users.noreply.github.com>
2020-01-22 12:46:34 -0500

    Update index.js

--- index.js
+++ index.js
@@ -6,16 +6,13 @@ function getEletronics() {
     .then((productsArr) => {
         productsArr.forEach((product) => {
             // console.log(product)
-            const { id, name, image } = product
-            const description = product["The Amiga is a family of personal computers introduced by Commodore in 1985. The original model was part of a wave of 16- and 32-bit computers that featured 256 KB or more of RAM, mouse-based GUIs, and significantly improved graphics and audio over 8-bit systems."]
+            const { id, name, image, description} = product
             new Electronic(id, name, image, description)
         })
     })
 }
 
 getEletronics()
-
-
 
 
 // // getReviews
